9865dad136bc803a20a0efdb9e559d9da61ef0e0
wxm <157215725@qq.com>
2025-02-21 10:45:58 +0800

    feat: update fill

--- src/interfaces/FillDeclaration.ts
+++ src/interfaces/FillDeclaration.ts
@@ -1,5 +1,30 @@
+export interface FillRect {
+  left?: number
+  top?: number
+  bottom?: number
+  right?: number
+}
+
+export interface FillTile {
+  alignment?: string
+  scaleX?: number
+  scaleY?: number
+  translateX?: number
+  translateY?: number
+  flip?: string
+}
+
 export interface FillDeclaration {
-  image?: string
-  color?: string
   opacity?: number
+
+  // blip
+  image?: string
+  dpi?: number
+  rotateWithShape?: boolean
+  srcRect?: FillRect
+  fillRect?: FillRect
+  tile?: FillTile
+
+  // solid / gradient
+  color?: string
 }
