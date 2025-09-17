bfa5f44d7586dd32a75c25545af9abc48632817a
wxm <157215725@qq.com>
2025-09-17 15:23:39 +0800

    feat: update Reactivable

--- src/utils/Reactivable.ts
+++ src/utils/Reactivable.ts
@@ -17,6 +17,31 @@ export interface Reactivable {
 export class Reactivable extends Observable implements PropertyAccessor {
   protected _propertyAccessor?: PropertyAccessor
   protected _properties = new Map<string, unknown>()
+  protected _updatedProperties = new Map<string, unknown>()
+  protected _changedProperties = new Set<string>()
+  protected _updatingPromise = Promise.resolve()
+  protected _updating = false
+
+  constructor(properties?: Record<string, any>) {
+    super()
+    this.setProperties(properties)
+  }
+
+  isDirty(key?: string): boolean {
+    return key
+      ? this._updatedProperties.has(key)
+      : this._updatedProperties.size > 0
+  }
+
+  offsetGet(key: string): any {
+    // @ts-expect-error ignore
+    return this[key]
+  }
+
+  offsetSet(key: string, newValue: any): void {
+    // @ts-expect-error ignore
+    this[key] = newValue
+  }
 
   getProperty(key: string, defaultValue?: any): any {
     if (this._propertyAccessor?.getProperty) {
@@ -28,8 +53,7 @@ export class Reactivable extends Observable implements PropertyAccessor {
   }
 
   setProperty(key: string, newValue: any): void {
-    // @ts-expect-error ignore
-    const oldValue = this[key]
+    const oldValue = this.offsetGet(key)
     if (Object.is(newValue, oldValue)) {
       return
     }
@@ -42,39 +66,145 @@ export class Reactivable extends Observable implements PropertyAccessor {
     this.onUpdateProperty(key, newValue, oldValue)
   }
 
+  getProperties(keys?: string[]): Record<string, any> {
+    const properties: Record<string, any> = {}
+    for (const [name, property] of this.getPropertyDeclarations()) {
+      if (!property.internal && !property.alias && (!keys || keys.includes(name))) {
+        properties[name] = this.getProperty(name)
+      }
+    }
+    return properties
+  }
+
+  setProperties(properties?: Record<string, any>): this {
+    if (properties && typeof properties === 'object') {
+      for (const [name] of this.getPropertyDeclarations()) {
+        if (name in properties) {
+          this.offsetSet(name, properties[name])
+        }
+      }
+    }
+    return this
+  }
+
+  resetProperties(): this {
+    for (const [name, property] of this.getPropertyDeclarations()) {
+      this.offsetSet(
+        name,
+        typeof property.default === 'function'
+          ? property.default()
+          : property.default,
+      )
+    }
+    return this
+  }
+
   getPropertyDeclarations(): Map<string, PropertyDeclaration> {
     return getDeclarations(this.constructor)
+  }
+
+  getPropertyDeclaration(key: string): PropertyDeclaration | undefined {
+    return this.getPropertyDeclarations().get(key)
   }
 
   setPropertyAccessor(accessor: PropertyAccessor): this {
     this._propertyAccessor = accessor
 
     this.getPropertyDeclarations().forEach((_declaration, key) => {
-      // @ts-expect-error ignore
-      const newValue = this[key]
+      const newValue = this.offsetGet(key)
       const oldValue = this._properties.get(key)
       if (newValue === undefined) {
         if (oldValue !== undefined) {
-          // @ts-expect-error ignore
-          this[key] = oldValue
+          this.offsetSet(key, oldValue)
         }
       }
       else if (newValue !== oldValue) {
-        // @ts-expect-error ignore
-        this[key] = newValue
+        this.offsetSet(key, newValue)
       }
     })
 
     return this
   }
 
+  protected async _nextTick(): Promise<void> {
+    if ('requestAnimationFrame' in globalThis) {
+      return new Promise(r => (globalThis as any).requestAnimationFrame(r))
+    }
+    return Promise.resolve()
+  }
+
+  protected async _enqueueUpdate(): Promise<void> {
+    this._updating = true
+    try {
+      await this._updatingPromise
+    }
+    catch (e) {
+      Promise.reject(e)
+    }
+    await this._nextTick()
+    if (!this._updating)
+      return
+    this.onUpdate()
+    this._updating = false
+  }
+
+  onUpdate(): void {
+    this._update(this._updatedProperties)
+    this._updatedProperties = new Map()
+  }
+
   onUpdateProperty(key: string, newValue: any, oldValue: any): void {
-    this._updateProperty(key, newValue, oldValue)
-    this.emit('updateProperty', key, newValue, oldValue)
+    this.requestUpdate(key, newValue, oldValue)
+  }
+
+  requestUpdate(key?: string, newValue?: any, oldValue?: any): void {
+    if (key !== undefined) {
+      this._updatedProperties.set(key, oldValue)
+      this._changedProperties.add(key)
+      this._updateProperty(key, newValue, oldValue)
+      this.emit('updateProperty', key, newValue, oldValue)
+    }
+
+    if (!this._updating) {
+      this._updatingPromise = this._enqueueUpdate()
+    }
+  }
+
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  protected _update(changed: Map<string, any>): void {
+    /** override */
   }
 
   // eslint-disable-next-line unused-imports/no-unused-vars
   protected _updateProperty(key: string, newValue: any, oldValue: any): void {
     /** override */
+  }
+
+  toJSON(): Record<string, any> {
+    const json: Record<string, any> = {}
+    this._properties.forEach((value, key) => {
+      if (value === undefined) {
+        return
+      }
+      if (value && typeof value === 'object') {
+        if ('toJSON' in value && typeof value.toJSON === 'function') {
+          json[key] = value.toJSON()
+        }
+        else if (Array.isArray(value)) {
+          json[key] = [...value]
+        }
+        else {
+          json[key] = { ...value }
+        }
+      }
+      else {
+        json[key] = value
+      }
+    })
+    return json
+  }
+
+  clone(): this {
+    return new (this.constructor as any)(this.toJSON())
   }
 }
